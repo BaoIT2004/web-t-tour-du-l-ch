d3b53953d19d7272bac801941cd5cdb6d1110803
Quân Phạm <pmqkks@gmail.com>
2025-11-29 20:01:14 +0700

    quanupdate

--- front-end/tourdulich/src/login.jsx
+++ front-end/tourdulich/src/login.jsx
@@ -1,15 +1,41 @@
 import { useState } from "react";
 import "./App.css";
+import { useNavigate  } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("Đăng nhập thành công");
-  };  
+    try {
+      const res = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",  //Header này cho server biết rằng dữ liệu bạn gửi trong body là JSON
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();  //Lấy dữ liệu JSON mà server trả về → chuyển thành object JavaScript.
+      console.log("Response từ server:", data);
+      if (data) {
+        //Lưu thông tin user
+        localStorage.setItem("user", JSON.stringify({
+          email: data.user.email
+        }));
+
+        navigate("/"); 
+      }
+    } catch (err) {
+      console.error("Lỗi khi gọi API:", err);
+      alert("Đăng nhập thất bại");
+    }
+  };
 
   return (
     <div className="login-page">
